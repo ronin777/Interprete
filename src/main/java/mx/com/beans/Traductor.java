137df346fbe167325f62ad8ac679c6cc451e4ebb
ronin <crazzy-rock@live.com.mx>
2016-01-06 08:17:43 -0600

    agregando la clase traductor

--- src/main/java/mx/com/beans/Traductor.java
+++ src/main/java/mx/com/beans/Traductor.java
@@ -2,4 +2,30 @@ package mx.com.beans;
 
 public class Traductor {
 
+	private InterpreteSpanish interprete;
+	private String nombre;
+	
+	public void hablar(){
+		this.interprete.saludar();
+		System.out.println(nombre);
+		this.interprete.despedirse();
+	}
+
+	public InterpreteSpanish getInterprete() {
+		return interprete;
+	}
+
+	public void setInterprete(InterpreteSpanish interprete) {
+		this.interprete = interprete;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	
 }
