24e5564b262f05fc7fa857fa429b15b9c083f4cd
ronin <crazzy-rock@live.com.mx>
2016-01-06 08:14:38 -0600

    primer commit para guardar la estructura del proyecto

---
+++ src/main/java/mx/com/beans/InterpreteSpanish.java
@@ -0,0 +1,13 @@
+package mx.com.beans;
+
+public class InterpreteSpanish {
+
+	public void saludar(){
+		System.out.println("Hola, mi nombre es ");
+	}
+	
+	public void despedirse(){
+		System.out.println("Hasta pronto... ");
+	}
+	
+}
---
+++ src/main/java/mx/com/beans/Traductor.java
@@ -0,0 +1,5 @@
+package mx.com.beans;
+
+public class Traductor {
+
+}
